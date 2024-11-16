29a3489b97d18aec398115c9d681723ea2060301
Aleksandra Dobrowolska <oladobrowolska@outlook.com>
2024-11-16 15:56:29 +0100

    feat: OpenAPI configuration

---
+++ src/main/java/com/example/my_car/configuration/openapi/OpenAPIConfig.java
@@ -0,0 +1,9 @@
+package com.example.my_car.configuration.openapi;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(info = @Info(title = "My Car"))
+public class OpenAPIConfig {
+
+}
