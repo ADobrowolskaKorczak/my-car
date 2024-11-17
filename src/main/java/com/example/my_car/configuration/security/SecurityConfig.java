8350f00f5b392865a410bcbe5fb8da64b91a503b
Aleksandra Dobrowolska <oladobrowolska@outlook.com>
2024-11-17 21:51:32 +0100

    feat: Add bean - SecurityFilterChain

--- src/main/java/com/example/my_car/configuration/security/SecurityConfig.java
+++ src/main/java/com/example/my_car/configuration/security/SecurityConfig.java
@@ -2,13 +2,34 @@ package com.example.my_car.configuration.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/auth/welcome", "/error", "/api/v*/registration/**").permitAll();
+                    auth.requestMatchers("/auth/user/**").hasRole("USER");
+                    auth.requestMatchers("/auth/admin/**").hasRole("ADMIN");
+                    auth.anyRequest().authenticated();
+                })
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
+                .logout(auth -> auth.deleteCookies("JSESSIONID")
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout")))
+                .build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
